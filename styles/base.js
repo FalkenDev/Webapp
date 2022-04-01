1e6d10fcb7c429de0455428fd8ed13cd4a4a6c5c
“FalkenDev” <kafa21@student.bth.se>
2022-04-01 23:51:27 +0100

    Added js files and inserted the styles / css

---
+++ styles/base.js
@@ -0,0 +1,31 @@
+// Styles
+
+// App.tsx
+
+// Home.tsx
+export const container = {
+    flex: 1,
+}
+
+export const base = {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingLeft: 12,
+    paddingRight: 12,
+}
+
+export const img = {
+    width: 385,
+    height: 260,
+    alignSelf: 'center', 
+}
+
+export const products = {
+    fontSize: 18,
+    paddingTop: 5,
+}
+// Pick.tsx
+
+// PickList.tsx
+
+// OrderList.tsx
---
+++ styles/typography.js
@@ -0,0 +1,14 @@
+export const title = {
+    color: '#33c',
+    fontSize: 42,
+    textAlign: 'center',
+};
+
+// stock.tsx
+export const header2 = {
+    color: '#333',
+    fontSize: 24,
+    borderBottomWidth: 2,
+    borderLeftColor: 'black',
+    marginTop: 10,
+}
