e4eccc576a2c86730f7499aac0252cb5aa689426
“FalkenDev” <kafa21@student.bth.se>
2022-04-13 00:01:23 +0100

    Inserted route, products and setProducts

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, ScrollView } from 'react-native';
+import { Image, Text, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import warehouse from '../assets/warehouse.jpg';
 import Stock from '../components/Stock';
 import { Base, Typography } from '../styles/index.js'; 
 
-export default function App() {
+export default function Home({route, products, setProducts}) {
   return (
     <SafeAreaView style={Base.container}>
       <ScrollView style={Base.base}>
         <Text style={Typography.title}>Lager-Appen</Text>
         <Image source={warehouse} style={Base.img} />
-        <Stock />
+        <Stock products={products} setProducts={setProducts}/>
         <StatusBar style="auto" />
       </ScrollView>
     </SafeAreaView>
