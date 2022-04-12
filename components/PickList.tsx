926237f31f7790bb26f11cfa6042e741b7717d45
“FalkenDev” <kafa21@student.bth.se>
2022-04-13 00:04:28 +0100

    Added Pick function | Button if stock is higher then amount | Style

--- components/PickList.tsx
+++ components/PickList.tsx
@@ -1,33 +1,53 @@
 import { View, Text, Button } from "react-native";
+import { useState, useEffect } from 'react';
 import orderModel from "../models/orders";
+import productModel from "../models/product";
+import { Base, Typography } from '../styles/index.js';
 
-export default function PickList({ route, navigation }) {
+export default function PickList({ route, navigation, setProducts }) {
+    console.log("Details (PickList)");
     const { order } = route.params;
+    const [productsList, setProductsList] = useState([]);
+
+    useEffect(async ()=> {
+        setProductsList(await productModel.getProducts());
+    }, []);
 
     async function pick() {
         await orderModel.pickOrder(order);
-        navigation.navigate("List");
+        setProducts(await productModel.getProducts());
+        navigation.navigate("List", { reload: true });
     }
 
+    let allInStock = true;
+
     const orderItemsList = order.order_items.map((item, index) => {
-        return <Text
+        if (item.stock < item.amount) {
+            allInStock = false;
+        }
+
+        console.log(allInStock);
+
+        return <Text style={Typography.details_text}
                 key={index}
                 >
-                    {item.name} - {item.amount} - {item.location}
+                    - {item.name} - {item.amount} - {item.location}
             </Text>;
     });
 
     return (
         <View>
-            <Text>{order.name}</Text>
-            <Text>{order.address}</Text>
-            <Text>{order.zip} {order.city}</Text>
+            <Text style={Typography.details_text}>Namn: {order.name}</Text>
+            <Text style={Typography.details_text}>Adress: {order.address}</Text>
+            <Text style={Typography.details_text}>Zip: {order.zip} {order.city}</Text>
 
-            <Text>Produkter:</Text>
+            <Text style={Typography.details_highlight}>Produkter:</Text>
 
             {orderItemsList}
-
-            <Button title="Plocka order" onPress={pick} />
+            {allInStock
+                ?<Button title="Plocka order" onPress={pick} />
+                : <Text style={Typography.warning}>Varor saknas på lager, ordern går inte att packas!</Text>
+            }
         </View>
     )
 };
