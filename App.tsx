0310273e04b0b3bbec5c29b3dbdb6d8611c0ab89
“FalkenDev” <kafa21@student.bth.se>
2022-04-21 03:57:23 +0100

    Added new tab screen logga in and if logged in change it to faktura tab screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import { Feather } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Home from "./components/Home";
-import Pick from "./components/Pick";
-import Delivery from "./components/Deliveries"
-import Deliveries from './components/Deliveries';
+import Home from './components/Home';
+import Pick from './components/Pick';
+import Delivery from './components/Deliveries';
+import Invoices from './components/Invoices';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Base } from './styles/index.js'; 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import Auth from "./components/auth/Auth";
+import authModel from "./models/auth";
 
 
 const Tab = createBottomTabNavigator();
@@ -16,11 +19,21 @@ const routeIcons = {
   "Lager": "home",
   "Plock": "box",
   "Inleveranser": "plus-circle",
+  "Faktura": "printer",
+  "Logga in": "user",
 };
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
   const [products, setProducts] = useState([]);
-  console.log("app.tsx");
+
+  useEffect(async () => {
+    setIsLoggedIn(await authModel.loggedIn());
+  }, []);
+
+  console.log(isLoggedIn);
+
+  console.log("------| app.tsx |------");
   return (
     <SafeAreaView style={Base.container}>
       <NavigationContainer>
@@ -44,6 +57,14 @@ export default function App() {
           <Tab.Screen name="Inleveranser">
             {() => <Delivery setProducts={setProducts}/>}
           </Tab.Screen>
+          {isLoggedIn
+            ?<Tab.Screen name="Faktura">
+              {() => <Invoices setIsLoggedIn={setIsLoggedIn} />}
+            </Tab.Screen>
+            :<Tab.Screen name="Logga in">
+              {() => <Auth setIsLoggedIn={setIsLoggedIn} />}
+            </Tab.Screen>
+          }
         </Tab.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
