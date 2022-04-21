c52d2f2e5023438eebfef50e9be68af7d0d3a5c6
“FalkenDev” <kafa21@student.bth.se>
2022-04-21 04:11:06 +0100

    Added storage.ts model to store JWT token from user, read token and delete token

---
+++ models/storage.ts
@@ -0,0 +1,32 @@
+// models/storage.ts
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storage = {
+    storeToken: async function storageToken(token:string) {
+        try {
+            const tokenAndDate = {
+                token: token,
+                date: new Date().getTime()
+            };
+            const jsonValue = JSON.stringify(tokenAndDate);
+
+            await AsyncStorage.setItem('@token', jsonValue);
+        } catch (e) {
+            // Error
+        }
+    },
+
+    readToken: async function readToken(): Promise<any> {
+        try {
+            const jsonValue = await AsyncStorage.getItem("@token");
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch (e) {
+            // Error
+        }
+    },
+    deleteToken: async function deleteToken() {
+        await AsyncStorage.removeItem('@token');
+    }
+};
+
+export default storage;
