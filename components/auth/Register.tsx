c13a152a9717b2d411c89062b2754180602cb70d
“FalkenDev” <kafa21@student.bth.se>
2022-05-03 00:19:36 +0100

    Added showMessage if mail and password have been added or not

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -2,6 +2,7 @@ import Auth from '../../interfaces/auth';
 import { useState } from 'react';
 import AuthModel from '../../models/auth';
 import AuthFields from './AuthFields';
+import { showMessage } from 'react-native-flash-message';
 
 export default function Register({ navigation }) {
     const [auth, setAuth] = useState<Partial <Auth>>({});
@@ -9,7 +10,16 @@ export default function Register({ navigation }) {
     async function doRegister() {
         if (auth.email && auth.password) {
             const result = await AuthModel.register(auth.email, auth.password);
-            navigation.navigate("Login");
+            if(result.type === "success") {
+                navigation.navigate("Login");
+            }
+            showMessage(result);
+        } else {
+            showMessage({
+                message: "Mail or password missing",
+                description: "Please enter both email and password",
+                type: "warning"
+            });
         }
     }
 
