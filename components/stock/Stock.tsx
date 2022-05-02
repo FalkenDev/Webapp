7a8678fc50b5a43495471c95a83361eea5413453
“FalkenDev” <kafa21@student.bth.se>
2022-05-03 00:21:04 +0100

    Split the stock and stocklist function to two components

--- components/stock/Stock.tsx
+++ components/stock/Stock.tsx
@@ -2,26 +2,7 @@ import { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { Base, Typography } from '../../styles/index.js';
 import productModel from "../../models/product.ts";
-
-function StockList({products, setProducts}) {
-  console.log("StockList");
-  useEffect(async () => {
-    setProducts(await productModel.getProducts());
-  }, []);
-  const list = products.map((product, index) => {
-  return <Text 
-          style={Base.products}
-          key={index}
-          >
-            { product.name } - { product.stock }st
-          </Text>});
-
-  return (
-    <View>
-      {list}
-    </View>
-  );
-}
+import StockList from "../stock/StockList"
 
 export default function Stock({products, setProducts}) {
   return (
